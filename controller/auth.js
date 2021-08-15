0d800570a46d6bf3621bc56e9c770bff1c7700f5
Kim234 <djehoaos@ajou.ac.kr>
2021-08-15 20:36:44 +0900

    add login/logout session

--- controller/auth.js
+++ controller/auth.js
@@ -2,10 +2,31 @@ import * as authRepository from '../data/auth.js';
 
 export async function login(req, res) {
   const { id, password } = req.body;
+  // console.log(id+password)
   const user = await authRepository.findById({id, password});
+  console.log(user)
 
-  if(!user) {
-    return res.status(401).json({ message: 'Invalid user or password' });
+  //로그인 성공
+  if(user.length!=0) {
+    req.session.loginData = user
+    console.log(req.session.loginData)
+    res.status(200).json({ user, message: 'Login success' });
   }
-  res.status(200).json({ user, message: 'Login success' });
+  //로그인 실패
+  else{
+    res.json({ message: 'Invalid user or password' });
+  }
+}
+
+export async function logout(req, res) {
+  console.log(req.session);
+  if(req.session){
+    console.log("세션 정보가 있습니다")
+    req.session.destroy(() => {
+      res.status(200).json({ message: true });
+    });
+  } else{
+    console.log("세션 정보가 없습니다.")
+  }
+  
 }
--- router/auth.js
+++ router/auth.js
@@ -4,7 +4,8 @@ import * as authController from '../controller/auth.js';
 
 const router = express.Router();
 
-// POST /auth
+// GET,POST /auth
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 export default router;
