3b3d370f38035a73288163ae16d403305016560c
jeong <7863z@naver.com>
2021-08-17 21:40:10 +0900

    encryption

--- controller/auth.js
+++ controller/auth.js
@@ -1,35 +1,30 @@
 import * as authRepository from '../data/auth.js';
+import bcrypt from 'bcrypt';
 
 export async function login(req, res) {
   const { id, password } = req.body;
-  // console.log(id+password)
-  const user = await authRepository.findById({id, password});
-  console.log(user);
-  console.log(req.session);
-
-  //로그인 성공
-  if(user.length!=0) {
-    req.session.loginData = user;
-    req.session.is_logined = true;
-    console.log(req.session);
-    req.session.save(()=> {
+  const user = await authRepository.findById({id});
+  bcrypt.compare(password, user[0].password, (err, same) => {
+    if(same) {
+      req.session.loginData = user;
+      req.session.is_logined = true;
+      console.log(req.session);
+      req.session.save(()=> {
       res.status(200).json({ user, message: 'Login success' });
-    })
-  }
-  //로그인 실패
-  else{
-    res.json({ message: '아이디 또는 패스워드가 틀렸습니다.' });
-  }
+      })
+    } else {
+      res.json({ message: '아이디 또는 패스워드가 틀렸습니다.' });
+    }
+  });
 }
 
 export async function logout(req, res) {
-  console.log(req.session);
   if(req.session){
     console.log("세션 정보가 있습니다");
     req.session.destroy(() => {
       res.status(200).json({ message: true });
     });
   } else{
-    console.log("세션 정보가 없습니다.");
+    //console.log("세션 정보가 없습니다.");
   }
 }
--- data/auth.js
+++ data/auth.js
@@ -3,6 +3,6 @@ import { db } from '../db/database.js';
 export async function findById(user) {
   const { id, password } = user;
   return db
-    .execute('SELECT * FROM auth WHERE id=? and password=?', [id, password])
+    .execute('SELECT * FROM auth WHERE id=?', [id])
     .then((result) => result[0]);
 }
